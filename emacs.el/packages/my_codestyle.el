6d1d8fbad1271a607a1f431bc815229537b75413
HunterOn <werther0331@gmail.com>
2013-12-13 22:40:44 +0800

    highlight trailing white blank and TAB , warning 80 col

--- emacs.el/packages/my_codestyle.el
+++ emacs.el/packages/my_codestyle.el
@@ -1,9 +1,11 @@
 ;;(require 'cc-mode)
 (require 'google-c-style)
+(require 'modeline-posn)
+
 (defun my-build-tab-stop-list (width)
   (let ((num-tab-stops (/ 80 width))
-    (counter 1)
-    (ls nil))
+	(counter 1)
+	(ls nil))
     (while (<= counter num-tab-stops)
       (setq ls (cons (* width counter) ls))
       (setq counter (1+ counter)))
@@ -16,7 +18,7 @@
   (setq c-basic-offset tab-width)
   (setq indent-tabs-mode nil) ;; force only spaces for indentation
   (c-set-offset 'substatement-open 0)
-;;  (c-set-offset 'arglist-intro c-lineup-arglist-intro-after-paren)
+  ;;  (c-set-offset 'arglist-intro c-lineup-arglist-intro-after-paren)
   )
 ;; google sytle is defined in above function
 ;;(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
@@ -41,9 +43,30 @@
                                        filename))
                 (setq indent-tabs-mode t)
                 (c-set-style "my-c-mode-common-hook")))))
-
 ;;(add-hook 'c++-mode-common-hook 'my-c-mode-common-hook)
 (add-hook 'c-mode-common-hook 'google-make-newline-indent)
+
+;; show whitespace/tab and delete trailing white space when saving
+(setq whitespace-style
+      '(face indentation::tab indentation::space tabs tab-mark trailing))
+(whitespace-mode 1)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; Sets show-trailing-whitespace as expected
+(custom-set-variables
+ '(show-trailing-whitespace t))
+
+;; Draw tabs with the same color as trailing whitespace
+(add-hook 'font-lock-mode-hook
+	  (lambda ()
+	    (font-lock-add-keywords
+	     nil
+	     '(("\t" 0 'trailing-whitespace prepend)))))
+
+;; 80 col
+(column-number-mode 1)
+(size-indication-mode 1)
+
 ;;; cpplint
 (defun cpplint ()
   "check source code format according to Google Style Guide"
